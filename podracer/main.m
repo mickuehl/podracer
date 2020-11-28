ad526781099810b539d98bd497c9344a2aa92133
Michael Kuehl <hello@txs.vc>
2020-11-28 23:00:47 +0100

    Initial Commit

---
+++ podracer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  podracer
+//
+//  Created by Michael Kuehl on 28.11.20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
